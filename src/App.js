90eb35f43176a62c92ed8dfcd17271b8d8a1cc04
Jabulani Sifundza <jjsifundza@gmail.com>
2022-10-29 20:14:53 +0200

    Model upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
+import {Canvas} from '@react-three/fiber';
 import Chicago from './components/Chicago';
 //import Met from './components/Met';
 //import Header from './components/Header';
 //import Footer from './components/Footer';
-
+import Model from './models/Model';
 
 function App() {	
-		
-
   return (
 	<>
+		<Canvas camera={{ position: [30, 20, 45], fov: 25 }}>
+			<Suspense fallback={null}>
+			<Model />
+			</Suspense>
+		</Canvas>
 		
-		<Chicago />
-		
-	
 	</>
 )
-}
+}	
 export default App;
