9e06cb6e0ff0da6b4ef25e8cb9d4eccd1fe3b70a
Jabulani Sifundza <jjsifundza@gmail.com>
2022-11-08 15:23:05 +0200

    Adding HTML over 3D model bug creation

--- src/galleryInModel.js
+++ src/galleryInModel.js
@@ -1,7 +1,7 @@
 import React, {Suspense} from 'react';
 import './App.css';
 import {Canvas} from '@react-three/fiber';
-import {useGLTF, PerspectiveCamera, OrbitControls, Loader} from '@react-three/drei';
+import {useGLTF, PerspectiveCamera, OrbitControls, Loader, Html} from '@react-three/drei';
 import Chicago from './components/Chicago';
 //import Met from './components/Met';
 //import Header from './components/Header';
@@ -14,6 +14,11 @@ export function Model(props) {
 	  <group {...props} dispose={null}>
 		<mesh geometry={nodes.Cove_Lights.geometry} material={materials.Spot_Lights} position={[0.05, 3.4, 6.54]} scale={0.1} />
 		<mesh geometry={nodes.Room.geometry} material={materials['Material.001']} />
+		<Html className="gallerWrapper" position={[0, 1, 0]} transform occlude>
+		<div>
+			<Chicago />
+		<div/>
+		</Html>
 	  </group>
 	)
   }
